40f2661301c99112b7b2742114872176f73158c1
49EHyeon42 <49ehyeon42@gmail.com>
2024-01-25 15:18:32 +0900

    init: project

---
+++ src/test/java/dev/ehyeon/jenkinstest/JenkinsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ehyeon.jenkinstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
